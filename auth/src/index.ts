854470df8711cdadbb846448162f795fa7ead9c6
Stephanie Carlyle <catfiadhaich1138@gmail.com>
2022-02-09 17:36:01 -0500

    Adding routes to the auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi There');
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
-    console.log("Auth Service is listening on 3000");
+    console.log("The auth Service is listening on 3000");
 });
